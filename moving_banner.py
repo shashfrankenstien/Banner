e97d7eeff995aa93b6cef967749517a7c031c332
Shashank Gopikrishna <shashank.gopikrishna@gmail.com>
2021-05-15 11:05:56 -0400

    convert to python 3

--- moving_banner.py
+++ moving_banner.py
@@ -24,18 +24,18 @@ def moving_banner(out, speed=0.1, length=50):
 	for offset in range(length+len(out[0])):
 		os.system('clear')
 
-		print "_"*(length+4)
-		print '|', " "*length, '|'
+		print("_"*(length+4))
+		print('|', " "*length, '|')
 
 		for row in range(7):
 			printString= (prefix+out[row])[offset:][:length]
 			if len(printString) < length:
 				printString = printString+(" "*(length-len(printString)))
-			print '|', printString, '|'
+			print('|', printString, '|')
 
-		print "o"*(length+4)
+		print("o"*(length+4))
 
-		print '\nPress Ctl+C to exit!'
+		print('\nPress Ctl+C to exit!')
 		time.sleep(speed)
 
 
@@ -44,7 +44,7 @@ if __name__=='__main__':
 	try:
 		length = sys.argv[1]
 	except: length = 50
-	message = raw_input("Enter something - ")
+	message = input("Enter something - ")
 	#length = raw_input("Enter screen width(20-177) - ")
 	message = message.upper()
 	output = convert(message)
@@ -58,10 +58,10 @@ if __name__=='__main__':
 
 
 
-# print '\n'
+# print('\n')
 # for row in output:
-# 	print row
-# print '\n'
+# 	print(row)
+# print('\n')
 
 
 
