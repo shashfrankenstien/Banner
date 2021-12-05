f204a727b74a5517a2913aa8f84fc06921eec0e3
Shashank Gopikrishna <shashank.gopikrishna@gmail.com>
2021-12-04 20:03:00 -0500

    switch to figlet using pyfiglet

--- README.md
+++ README.md
@@ -2,23 +2,45 @@
 
 Converts string to a banner that moves on the console from right to left
 
+# Usage
+
+```
+$ python -m pip install -r requirements.txt
+```
 ```
 $ python moving_banner.py -h
 
-usage: python moving_banner.py [-h] [-c C] [-x X] [-y Y] [--width WIDTH] [--speed SPEED] [--quiet] message
+usage: python moving_banner.py [-h] [--figlet FIGLET] [--figlet-list] [-x X] [-y Y] [--width WIDTH] [--speed SPEED] [--quiet] [message]
 
 positional arguments:
-  message               message to print as banner
+  message               message to print as banner (default: None)
 
 optional arguments:
   -h, --help            show this help message and exit
-  -c C                  character used for banner text
-  -x X                  horizontal wall character
-  -y Y                  vertical wall character
+  --figlet FIGLET, -f FIGLET
+                        select figlet font name (default: banner)
+  --figlet-list, -fl    list available figlet fonts (default: False)
+  -x X                  horizontal wall character (default: _)
+  -y Y                  vertical wall character (default: |)
   --width WIDTH, -w WIDTH
-                        width of the banner
+                        width of the banner (default: 269)
   --speed SPEED, -s SPEED
-                        speed of the banner
-  --quiet, -q           don't print extra text
+                        speed of the banner (default: 50)
+  --quiet, -q           don't print extra text (default: False)
 
 ```
+
+# Change Log
+
+### v1.0.0 - 2016-05-04
+- Initial check-in
+- Draw border
+
+
+### v2.0.0 - 2021-12-04
+- Convert to Python3
+- Add CLI arguments
+
+
+### v3.0.0 - 2021-12-04
+- Switch to Figlet fonts using pyfiglet
--- moving_banner.py
+++ moving_banner.py
@@ -1,49 +1,49 @@
 #!/usr/bin/python
 
-__version__ = "2.0.0"
+__version__ = "3.0.0"
 
 import os, time
-from alphabet import alphabet, alphabet_char
+import pyfiglet
 import argparse
 
 TERM_WIDTH, _ = os.get_terminal_size()
 
-parser = argparse.ArgumentParser("python {}".format(os.path.basename(__file__)))
-parser.add_argument("message", help="message to print as banner")
-parser.add_argument("-c", help="character used for banner text", type=str, default=alphabet_char)
+parser = argparse.ArgumentParser("python {}".format(os.path.basename(__file__)), formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+parser.add_argument("message", help="message to print as banner", type=str, nargs='?', default=None)
+parser.add_argument("--figlet", "-f", help="select figlet font name", type=str, default="banner")
+parser.add_argument("--figlet-list", "-fl", help="list available figlet fonts", action="store_true")
 parser.add_argument("-x", help="horizontal wall character", type=str, default='_')
 parser.add_argument("-y", help="vertical wall character", type=str, default='|')
 parser.add_argument("--width", "-w", help="width of the banner", type=int, default=TERM_WIDTH)
 parser.add_argument("--speed", "-s", help="speed of the banner", type=int, default=50)
 parser.add_argument("--quiet", "-q", help="don't print extra text", action="store_true")
 
+
 def clear_scr():
 	os.system('cls' if os.name == 'nt' else 'clear')
 
-def convert(message, replace_char):
-	output = []
-	for i in range(7):
-		outputTemp=''
-		for char in str(message).upper():
-			outString = alphabet[char][i]+"  "
-			if replace_char and replace_char != alphabet_char:
-				outString = outString.replace(alphabet_char, str(replace_char)[0])
-			outputTemp += outString
-		output.append(outputTemp)
-	return output
 
-def moving_banner():
-	args = parser.parse_args()
+def moving_banner(args):
+	if args.message is None:
+		parser.print_help()
+		exit(1)
+
 	v_char = args.y
 	h_char = args.x
 	speed = 1/args.speed
-	width = min(args.width, TERM_WIDTH)
+	width = max(min(args.width, TERM_WIDTH), 10)
 	h_chars_all = h_char * int((width)/len(h_char))
 	width -= (2*len(v_char)) + 2 # reducing width to make room for extra characters
 
 	extras = 'Banner! v{}\nPress Ctl+C to exit!'.format(__version__)
 
-	out = convert(args.message, args.c)
+	try:
+		fig = pyfiglet.Figlet(font=args.figlet)
+	except pyfiglet.FontNotFound as e:
+		raise Exception("Figlet font {} not found. Use --figlet-list/-fl option to see available fonts".format(args.figlet)) from e
+	# convert out to list containing rows of printable characters
+	out = [o for o in fig.renderText(str(args.message)).split("\n") if o.strip() != ""]
 	clear_scr()
 
 	prefix = " "*width
@@ -54,8 +54,8 @@ def moving_banner():
 			print(h_chars_all)
 			print(v_char, " "*width, v_char)
 
-			for row in range(7):
-				printString= (prefix+out[row])[offset:][:width]
+			for row in out:
+				printString= (prefix+row)[offset:][:width]
 				if len(printString) < width:
 					printString = printString+(" "*(width-len(printString)))
 				print(v_char, printString, v_char)
@@ -67,10 +67,14 @@ def moving_banner():
 
 
 if __name__=='__main__':
-	try:
-		moving_banner()
-	except KeyboardInterrupt:
-		print("Exit!")
-	except Exception as e:
-		print(e)
-
+	args = parser.parse_args()
+	if args.figlet_list:
+		for f in pyfiglet.FigletFont.getFonts():
+			print(f)
+	else:
+		try:
+			moving_banner(args)
+		except KeyboardInterrupt:
+			print("Exit!")
+		except Exception as e:
+			print(e)
